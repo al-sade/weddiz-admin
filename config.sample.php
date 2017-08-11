d1256f699beaa6839fb5d3a44db32f8dc9c5e6e0
al-sade <alsade15@gmail.com>
2017-08-11 09:54:38 +0300

    sample config file

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+define('HOST', 'DB_HOST');
+define('USER', 'DB_USER');
+define('PASS', 'DB_PASS');
+define('DB', 'DB_NAME');
+
+define('BASEURL', 'LOCAL_BASE_ULR');
+define('LOCAL', 'ROOT_DIR');
+define('LOCALIMG', 'base_url./assets/uploads/');
+?>
