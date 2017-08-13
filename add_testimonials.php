35bcde7ae3506add4fe43974675cef381693ef88
Al <alsade15@gmail.com>
2017-08-14 01:49:42 +0300

    testimonials submit

--- add_testimonials.php
+++ add_testimonials.php
@@ -1,10 +1,16 @@
-<?php require_once('head.php'); ?>
+<?php
+require_once('head.php');
+$suppliers= $auth_admin->getAllSuppliers();
+?>
 
 <?php
 if(isset($_POST['submit'])){
-    $new_status = strip_tags($_POST['new_status']);
+    $supplier_id = strip_tags($_POST['supplier_id']);
+    $the_couple = strip_tags($_POST['the_couple']);
+    $event_date = strip_tags($_POST['event_date']);
+    $reco_text = strip_tags($_POST['supplier_id']);
     try{
-    $auth_admin->updateEventStatus($event_id, $new_status);
+    $auth_admin->saveTestimonial($supplier_id, $the_couple, $event_date, $reco_text);
     }catch (Exception $e) {
 		echo $e->getMessage();
 	}
@@ -23,21 +29,47 @@ if(isset($_POST['submit'])){
                             <form method="post" class="form-horizontal">
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">טלפון</label>
+                                <label class="col-sm-2 control-label">בחר ספק</label>
+                                <div class="col-sm-10">
+                                    <select class="form-control m-b" name="supplier_id">
+                                            <?php
+                                                foreach($suppliers as $key => $supplier){
+                                                      $option = '<option value="'.$supplier['supplier_id'].'">';                                                      
+                                                      $option .= $supplier['first_name'].' '.$supplier['last_name'];
+                                                      $option .= '</option>';
+                                                      echo $option;
+                                                    }
+                                            ?>
+                                            </select>
+                                    </div>
+                                </div>
+                                <div class="hr-line-dashed"></div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">שם הזוג</label>
                                     <div class="col-sm-10">
                                         <div class="row">
                                             <div class="col-md-4">
-                                                <input type="text" name="phone" placeholder="טלפון" class="form-control"> </div>
+                                                <input type="text" name="the_couple" placeholder="שם הזוג" class="form-control"> </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                 <div class="hr-line-dashed"></div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">תאריך האירוע</label>
+                                    <div class="col-sm-10">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <input type="date" name="event_date" placeholder="תאריך האירוע" class="form-control"> </div>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group">
-                                    <label class="col-sm-2 control-label">טלפון</label>
+                                    <label class="col-sm-2 control-label">המלצה</label>
                                     <div class="col-sm-10">
                                         <div class="row">
                                             <div class="col-md-4">
-                                                <input type="text" name="phone" placeholder="טלפון" class="form-control"> </div>
+                                                <textarea type="text" name="reco_text" rows="5" placeholder="כתוב המלצה" class="form-control"> </textarea></div>
                                         </div>
                                     </div>
                                 </div>
